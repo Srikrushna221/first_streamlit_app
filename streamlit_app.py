83330c7e2a46e248c4d4194a20d62997c7442ee8
Srikrushna221 <61017540+Srikrushna221@users.noreply.github.com>
2023-06-02 15:41:04 +0530

    Update streamlit_app.py
    Create Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents new healthy diner')
-streamlit.title('Intentional bug)
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
